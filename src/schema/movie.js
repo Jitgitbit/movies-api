b701454310be69bf0104de59066357c0a10a8516
Jitgitbit <rowowolf@gmail.com>
2020-02-13 11:13:46 +0100

    preparing fetch data form external API

---
+++ src/schema/movie.js
@@ -0,0 +1,17 @@
+// src/schema/movie.js
+import { makeExecutableSchema } from 'graphql-tools';
+const typeDefs = `
+  type Query {
+    movie(id: ID): Movie
+  }
+  type Movie {
+    id: ID!
+  }
+`;
+const resolvers = {
+};
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
+export default schema;
