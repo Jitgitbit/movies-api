98bc1bff24581cf463f89d8667cb456e22a82435
Jitgitbit <rowowolf@gmail.com>
2020-02-13 12:05:14 +0100

    added a type and enum

--- src/schema/movie.js
+++ src/schema/movie.js
@@ -6,6 +6,22 @@ const typeDefs = `
   }
   type Movie {
     id: ID!
+    title: String!
+    budget(currency: Currency = EUR): Int
+    release_date: String!
+    production_companies: [Production_companies]
+  }
+
+  enum Currency {
+    EUR
+    GBP
+    USD
+  }
+  type Production_companies {
+    id: ID!
+    logo_path: String!
+    name: String!
+    origin_country: String!
   }
 `;
 const resolvers = {
